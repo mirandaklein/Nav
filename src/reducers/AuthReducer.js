93b299b724b370d4fa4fd783eda619544a651eb6
mirandaklein <mirandaklein@mac.com>
2019-03-13 14:54:48 -0600

    loginUser action to reducer

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,10 +1,12 @@
 import { EMAIL_CHANGED,
-         PASSWORD_CHANGED
+         PASSWORD_CHANGED,
+         LOGIN_USER_SUCCESS
 } from '../actions/types.js'
 
 const INITIAL_STATE = { 
     email: '',
-    password: ''
+    password: '',
+    user: ''
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -15,6 +17,8 @@ export default (state = INITIAL_STATE, action) => {
             return { ...state, email: action.payload };
         case PASSWORD_CHANGED:
             return { ...state, password: action.payload };
+        case LOGIN_USER_SUCCESS:
+            return { ...state, user: action.payload}
         default: 
             return state;
     }
