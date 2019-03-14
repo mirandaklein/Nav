112610dd44fc2a1a3ce57d3cfdc084f17c7cb123
mirandaklein <mirandaklein@mac.com>
2019-03-14 11:50:08 -0600

    employeeCreate wired to action

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -3,19 +3,28 @@ import Card from './Card.js';
 import CardItem from './CardItem.js';
 import Input from './Input.js';
 import Button from './Button.js';
+import { connect } from 'react-redux';
+import { employeeUpdate } from '../actions';
+
 
 class EmployeeCreate extends Component{
     render(){
         return(
             <Card>
-                <CardItem 
+                <CardItem>
+                    <Input
                     label='Name'
                     placeholder='Miranda'
-                />
+                    value={this.props.name}
+                    onChangeText={text => this.props.employeeUpdate({ prop: 'name', value: text })}
+                    />
+                </CardItem>
                 <CardItem>
                     <Input 
                     label='Phone'
                     placeholder='555-555-555'
+                    value={this.props.phone}
+                    onChangeText={text => this.props.employeeUpdate({ prop: 'phone', value: text })}
                     />
                 </CardItem>
                 <CardItem>
@@ -27,4 +36,9 @@ class EmployeeCreate extends Component{
         );
     }
 }
-export default EmployeeCreate;
+
+const mapStateToProps = (state) => {
+    const { name, phone, shift } = state.employeeForm;
+    return { name, phone, shift };
+}
+export default connect (mapStateToProps, { employeeUpdate })(EmployeeCreate);
